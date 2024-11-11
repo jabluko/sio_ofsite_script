f7d324970b3e02a0b0c626c91deff9d892ea1347
jabluko <cs.pawelmieszkowski@gmail.com>
2024-11-11 20:39:31 +0100

    style

--- script.sh
+++ script.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-if [[ "$#" -ne 3 ]]; then
-    echo -n "script usage: "
-    echo -n $(basename "$0")
-    echo " [skrót zadania] [nazwa zadania] [link do zadania]"
+if test "$#" -ne 3; then
+    echo "Poprawne użycie skryptu: "$0" [skrót zadania] [nazwa zadania] [link do zadania]"
+    echo -n "Uwaga! Jeżeli w nazwie lub linku do zadania występują spacje lub znaki"
+    echo " specjalne to należy otoczyć odpowiednio nazwę lub link cudzysłowiem."
     exit 1
 fi
+
 
 mkdir $1
 cd $1
 
 mkdir in
-touch "in/${1}1.in"
+touch "in/"$1"1.in"
 
 mkdir out
-echo "Zrobione" > "out/${1}1.out"
+echo "Zrobione" > "out/"$1"1.out"
 
-echo "title: ${2}" >"config.yml"
+echo "title: "$2 >"config.yml"
 
 mkdir doc
 cd doc
@@ -34,12 +35,11 @@ pom3='">Redirect</a></p>
     </body>
 </html>'
 
-echo "${pom1}${3}${pom2}${3}${pom3}" >index.html
-zip "${1}zad.html.zip" index.html >/dev/null
+echo $pom1$3$pom2$3$pom3 >index.html
+zip $1"zad.html.zip" index.html >/dev/null
 rm index.html
 
 cd ../..
-zip -r "${1}.zip" "${1}" >/dev/null
+zip -r $1".zip" $1 >/dev/null
 
-rm -r "${1}"
-            
+rm -r $1
